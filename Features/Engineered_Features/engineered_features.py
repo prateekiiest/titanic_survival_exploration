bb5e61bc3392b1474a4b0fdab03ba57fcbf23cf0
shreyas-kowshik <shreyaskowshik@gmail.com>
2017-12-20 12:54:11 +0530

    Tried to improve code quality

--- Features/Engineered_Features/engineered_features.py
+++ Features/Engineered_Features/engineered_features.py
@@ -43,22 +43,16 @@ gt_test = combined.iloc[891:].drop(features_to_drop,axis = 1).groupby(['Sex','Pc
 
 
 def fillAges(row, grouped_median):
+ return (grouped_median.loc[row['Sex'],row['Pclass'],row['Title']]['Age']) 
 
- Pclass = [1,2,3];
- Sex = ['male','female'];
- Title = ['Miss','Mrs','Officer','Royalty','Master','Mr'];
-
- for pclass in Pclass:
-  for sex in Sex:
-   for title in Title:
-    if(row['Sex']==sex and str(row['Pclass'])==str(pclass) and row['Title']==title):
-     return (int)(grouped_median.loc[row['Sex'],row['Pclass'],row['Title']]['Age'])
-
-combined.head(891).Age = combined.head(891).apply(lambda r : fillAges(r, gt_train) if np.isnan(r['Age']) 
-                                                      else r['Age'], axis=1)
-    
-combined.iloc[891:].Age = combined.iloc[891:].apply(lambda r : fillAges(r, gt_test) if np.isnan(r['Age']) 
-                                                      else r['Age'], axis=1)
+def fillTrainAge(combined):
+	combined.head(891).Age = combined.head(891).apply(lambda r : fillAges(r, gt_train) if np.isnan(r['Age']) 
+	                                                      else r['Age'], axis=1)
+def fillTestAge(combined):
+	combined.iloc[891:].Age = combined.iloc[891:].apply(lambda r : fillAges(r, gt_test) if np.isnan(r['Age']) 
+	                                                      else r['Age'], axis=1)
+fillTrainAge(combined)
+fillTestAge(combined)
 
 combined['Fare'] = combined['Fare'].fillna(combined.Fare.mean())
 
@@ -82,36 +76,12 @@ combined['Singleton'] = combined['FamilySize'].map(lambda s: 1 if s == 1 else 0)
 combined['SmallFamily'] = combined['FamilySize'].map(lambda s: 1 if 2<=s<=4 else 0)
 combined['LargeFamily'] = combined['FamilySize'].map(lambda s: 1 if 5<=s else 0)
 
-#Encode all variables
-pclass_dummies = pd.get_dummies(combined['Pclass'], prefix="Pclass")
-combined = pd.concat([combined,pclass_dummies],axis=1)
-combined.drop('Pclass',axis=1,inplace=True)
-
-#Encode all variables
-s_dummies = pd.get_dummies(combined['Sex'], prefix="Sex")
-combined = pd.concat([combined,s_dummies],axis=1)
-combined.drop('Sex',axis=1,inplace=True)
-
-#Encode all variables
-t_dummies = pd.get_dummies(combined['Ticket'], prefix="Ticket")
-combined = pd.concat([combined,t_dummies],axis=1)
-combined.drop('Ticket',axis=1,inplace=True)
-
-
-#Encode all variables
-e_dummies = pd.get_dummies(combined['Embarked'], prefix="Embarked")
-combined = pd.concat([combined,e_dummies],axis=1)
-combined.drop('Embarked',axis=1,inplace=True)
-
-#Encode all variables
-c_dummies = pd.get_dummies(combined['Cabin'], prefix="Cabin")
-combined = pd.concat([combined,c_dummies],axis=1)
-combined.drop('Cabin',axis=1,inplace=True)
-
-#Encode all variables
-t_dummies = pd.get_dummies(combined['Title'], prefix="Title")
-combined = pd.concat([combined,t_dummies],axis=1)
-combined.drop('Title',axis=1,inplace=True)
+#encode all the categorical variables
+features = ['Pclass','Sex','Ticket','Embarked','Cabin','Title']
+for feature in features:
+	df_encoded = pd.get_dummies(combined[feature], prefix=feature)
+	combined = pd.concat([combined,df_encoded],axis = 1)
+	combined.drop(feature,axis=1,inplace=True)
 
 combined.to_csv('titanic_engineered.csv')
 
--- Illustrations/Embarked/Embarked_Vs_Survived.py
+++ Illustrations/Embarked/Embarked_Vs_Survived.py
@@ -3,8 +3,7 @@ from matplotlib import pyplot as plt
 
 data = pd.read_csv('titanic_data.csv')
 
-emb_died = data[data['Survived']==0]['Embarked'].value_counts()
-emb_survived = data[data['Survived']==1]['Embarked'].value_counts()
-comb = pd.DataFrame([emb_died,emb_survived])
+comb = pd.DataFrame([data[data['Survived']==0]['Embarked'].value_counts(),
+	data[data['Survived']==1]['Embarked'].value_counts()])
 comb.index = ['Dead','Survived']
 comb.plot(kind = 'bar',stacked = True)
